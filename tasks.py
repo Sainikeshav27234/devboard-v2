5fc9cd40c9f57ba28646fe8e68ed4484bcb7c539
Sreejit Hore <sreejit2006hore@gmail.com>
2026-03-05 10:44:58 +0530

    added delete_task fn

--- tasks.py
+++ tasks.py
@@ -11,3 +11,9 @@ def display_tasks():
 
     for t in tasks:
         print(f"[{t['id']}] {t['title']} ({t['priority']})") 
+
+def delete_task(task_id): 
+global tasks original_len = len(tasks) 
+tasks = [t for t in tasks if t["id"] != task_id] 
+	if len(tasks) < original_len: print(f"🗑️ Task {task_id} deleted.") 
+	else: print(f"❌ No task found with ID {task_id}")
